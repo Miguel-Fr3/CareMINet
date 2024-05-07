69f19c21771aacacf068e15e79abb84340981398
Miguel_Fr3 <miguel.fernandes2004f2@gmail.com>
2024-05-07 15:05:56 -0300

    Adicionando model

---
+++ CareMI/Models/Usuario.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CareMI.Models
+{
+    [Table("t_usuario")]
+    public class Usuario
+    {
+        [Key]
+        public int cdUsuario { get; set; }
+        public String nmUsuario { get; set; }
+        public DateOnly dtNascimento { get; set; }
+        public String nrCpf { get; set; }
+        public String nrRg { get; set; }
+        public String dsNacionalidade { get; set;}
+        public String nrTelefone { get; set; }
+        public DateOnly dtCadastro { get; set; }
+        public String dsEstadoCivil { get;set; }
+        public String dsProfissao { get; set; }
+        public int fgAtivo { get; set; }
+    }
+}
